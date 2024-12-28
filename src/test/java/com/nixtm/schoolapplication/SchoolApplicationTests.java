8767d190fd56c8ec79d306c8a4ca8b9bf9b094f4
ANUJ NIRVAL <anuj.nirvalweb@gmail.com>
2024-12-28 18:57:17 +0530

    Initial Commit

---
+++ src/test/java/com/nixtm/schoolapplication/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nixtm.schoolapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
