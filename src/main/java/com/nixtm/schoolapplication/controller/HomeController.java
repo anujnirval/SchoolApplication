42e3e86b8aec9dbf06b7758ee10e2fd544dfabcf
ANUJ NIRVAL <anuj.nirvalweb@gmail.com>
2024-12-29 17:05:52 +0530

    Initial Commit

--- src/main/java/com/nixtm/schoolapplication/controller/HomeController.java
+++ src/main/java/com/nixtm/schoolapplication/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.nixtm.schoolapplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String displayHomePage() {
+    @RequestMapping(value={"","/","/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("title", "School Application");
+        model.addAttribute("username","ANUJ NIRVAL");
         return "home.html";
     }
 }
